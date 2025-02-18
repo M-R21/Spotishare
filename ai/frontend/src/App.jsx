db13aad7f4fde23fa783e6e7570c4be62c26a115
Shadow21mr <shadow21klx@gmail.com>
2025-02-18 21:20:45 +0100

    added dynamical product view

--- ai/frontend/src/App.jsx
+++ ai/frontend/src/App.jsx
@@ -1,11 +1,18 @@
 
 import './App.css'
 import MovieCard from './components/MovieCard'
+
+
 function App() {
+  const movieNumber =3;
+
 
   return (
     <>
-    <MovieCard movie={{title:"first movie" , release_Date:"2025"}}/>
+    {movieNumber === 1 ? (
+    <MovieCard movie={{title:"first movie" , release_Date:"2025"}}/>) 
+    : ( <h1> No movies to show</h1>)}   
+
     </>
 
 
